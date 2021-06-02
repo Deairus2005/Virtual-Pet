27fd889d27fcae100509f680440c08a611ccfc4d
Pearl-softdev <76012276+Pearl-softdev@users.noreply.github.com>
2021-06-02 13:15:54 -0400

    Pearl's Changes

--- VirtualPet/Pet.cs
+++ VirtualPet/Pet.cs
@@ -5,6 +5,36 @@ using System.Text;
 namespace VirtualPet
 {
     public class Pet
+
     {
+        private string Name
+        {
+            get; set;
+        }
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public string GetName()
+        {
+            return Name;
+
+        }
+
+        public string Species { get; set; }
+
+        public void SetSpecies(string species)
+        {
+            Species = species;
+        }
+
+        public string GetSpecies()
+        {
+            return Species;
+
+        }
+
     }
+
 }
--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -7,6 +7,10 @@ namespace VirtualPet
         static void Main(string[] args)
         {
             Console.WriteLine("Hello! Welcome to Virtual Pets");
+            Pet pet1 = new Pet();
+            Console.WriteLine("Enter your pet's name");
+            string userInput = Console.ReadLine();
+            pet1.SetName  (userInput);
         }
     }
 }
